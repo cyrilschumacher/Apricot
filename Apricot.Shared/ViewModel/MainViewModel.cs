7dd9f14d810902a507e3d928832159515d7856ba
Cyril Schumacher <cyrilschumacher>
2015-05-21 20:38:11 +0200

    Add project.

---
+++ Apricot.Shared/ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Apricot.Shared.ViewModel
+{
+    /// <summary>
+    ///     ViewModel for Main View.
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+    }
+}
---
+++ Apricot/View/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml.Navigation;
+
+namespace Apricot.View
+{
+    /// <summary>
+    ///     Main View.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Required;
+        }
+    }
+}
