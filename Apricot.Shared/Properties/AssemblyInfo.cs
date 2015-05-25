fea0e7ff6a4afedaaf1f0dee868b63d1a1878c95
Cyril Schumacher <cyrilschumacher>
2015-05-25 19:53:31 +0200

    Add CLS compliance for "Apricot.Shared" project. Clear AssemblyInfo.cs for all projects.

--- Apricot.Shared/Properties/AssemblyInfo.cs
+++ Apricot.Shared/Properties/AssemblyInfo.cs
@@ -1,9 +1,8 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+[assembly: CLSCompliant(true)]
 [assembly: AssemblyTitle("Apricot.Shared")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -12,17 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
