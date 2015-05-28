1d22deae704f91a54fa3f9405f2367773a5810f4
Cyril Schumacher <cyril_schumacher@live.fr>
2015-05-28 10:53:32 +0200

    Add a service for obtains alert (watering).

---
+++ Apricot.Shared/Models/Services/AlertServiceModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Apricot.Shared.Models.Services
+{
+    /// <summary>
+    ///     Model for the time remaining before watering.
+    /// </summary>
+    public class AlertServiceModel
+    {
+        #region Properties.
+
+        /// <summary>
+        ///     Gets or sets a time remaining.
+        /// </summary>
+        [JsonProperty("time")]
+        [JsonConverter(typeof(IsoDateTimeConverter))]
+        public DateTime LeftTime { get; set; }
+
+        #endregion Properties.
+    }
+}
---
+++ Apricot.Shared/Services/AlertService.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Apricot.Shared.Models.Services;
+using Apricot.Shared.Services.Apricot;
+
+namespace Apricot.Shared.Services
+{
+    /// <summary>
+    ///     Service for manage alert.
+    /// </summary>
+    public class AlertService : AbstractApricotService
+    {
+        #region Constants.
+
+        /// <summary>
+        ///     Service name for obtains the time remaining before the next watering is done.
+        /// </summary>
+        private const string GetAlertServiceName = "getAlert/watering/{0}";
+
+        #endregion Constants.
+
+        #region Methods.
+
+        /// <summary>
+        ///     Gets the alert by <paramref name="plantIdentifier"/>.
+        /// </summary>
+        /// <param name="plantIdentifier">The plant identifier.</param>
+        /// <returns>The time remaining before watering.</returns>
+        public async Task<AlertServiceModel> GetByPlantIdentifierAsync(int plantIdentifier)
+        {
+            var serviceUri = string.Format(GetAlertServiceName, plantIdentifier);
+            return await GetAsync<AlertServiceModel>(serviceUri);
+        }
+
+        #endregion Methods.
+    }
+}
